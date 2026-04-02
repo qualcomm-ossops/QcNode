f1670f4f157d21c66cf5e5fb142db87d7d630f71
Parai Wang <fpwang@qti.qualcomm.com>
2026-04-02 15:00:36 +0530

    3rd party: change SDL2_gfx-1.0.4.tar.gz source url to the stable sourceforge
    1. Change SDL2_gfx-1.0.4.tar.gz source url to the stable sourceforge.
    2. The one on ferzkopp has download error issue on some PC.
    
    Change-Id: Ia3d660aed1931caba804124dc6a6e431ea2ab927
    Signed-off-by: Parai Wang <fpwang@qti.qualcomm.com>

--- scripts/build/toolchain/get-3rd-party.sh
+++ scripts/build/toolchain/get-3rd-party.sh
@@ -38,7 +38,7 @@ qcdownload() {
 
 if [ "$ENABLE_TINYVIZ" == "ON" ] ; then
   qcdownload https://www.libsdl.org/release/SDL2-2.0.14.tar.gz SDL2-2.0.14.tar.gz 76ed4e6da9c07bd168b2acd9bfefab1b
-  qcdownload http://www.ferzkopp.net/Software/SDL2_gfx/SDL2_gfx-1.0.4.tar.gz SDL2_gfx-1.0.4.tar.gz 15f9866c6464ca298f28f62fe5b36d9f
+  qcdownload https://sourceforge.net/projects/sdl2gfx/files/SDL2_gfx-1.0.4.tar.gz SDL2_gfx-1.0.4.tar.gz 15f9866c6464ca298f28f62fe5b36d9f
   qcdownload https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.15.tar.gz SDL2_ttf-2.0.15.tar.gz 04fe06ff7623d7bdcb704e82f5f88391
   qcdownload https://dl.dafont.com/dl/?f=liberation_sans liberation_sans.zip c553a360214638956a561ce5538e49cc
 fi # ENABLE_TINYVIZ
